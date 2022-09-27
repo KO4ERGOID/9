73d5c3d928cd86383904d0a231b925e13d5b5252
KO4ERGOID <108968047+KO4ERGOID@users.noreply.github.com>
2022-09-27 16:22:41 +0700

    Delete .description.php

--- .description.php
+++
@@ -1,8 +0,0 @@
-<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-$arComponentDescription = array(
-    'NAME' => 'Авто сотрудников',
-    'DESCRIPTION' => 'Компонент для вывода информации об автомобилях сотрудников',
-    'PATH' => array(
-        'ID' => 'Авто сотрудников',
-    )
-);
