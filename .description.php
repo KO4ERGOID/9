e6e8705063fd009d33e6fe46bf6d333dda23f7d9
KO4ERGOID <108968047+KO4ERGOID@users.noreply.github.com>
2022-09-27 16:23:26 +0700

    Add files via upload

---
+++ .description.php
@@ -0,0 +1,8 @@
+<? if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+$arComponentDescription = array(
+    'NAME' => 'Авто сотрудников',
+    'DESCRIPTION' => 'Компонент для вывода информации об автомобилях сотрудников',
+    'PATH' => array(
+        'ID' => 'Авто сотрудников',
+    )
+);
